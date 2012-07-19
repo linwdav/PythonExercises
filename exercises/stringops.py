659e7eb5fe6d670b2e413bb8f9bc035cdc71a63f
David Lin <linwdav@gmail.com>
2012-07-18 18:18:07 -0700

    Add is_substring, used docstrings and dictionary

--- exercises/stringops.py
+++ exercises/stringops.py
@@ -1,12 +1,57 @@
+# O(n)
 def reverse(string):
+  """
+  Reverse a string.
+  @param string: Input string
+  @return: Reversed string
+  """
   result = ''
   for i in range(len(string) - 1, -1, -1):
     result = result + string[i]
   return result
 
+# O(n^2)
+#def has_unique_chars(string):
+#  """
+#  Determine if string has all unique characters.
+#  @param string: Input string
+#  @return: True if string has all unique characters, otherwise False
+#  """
+#  for i in range(0, len(string) - 1, 1):
+#    for j in range(i + 1, len(string), 1):
+#      if string[i] == string[j]:
+#        return False
+#  return True
+
+# O(n)
 def has_unique_chars(string):
-  for i in range(0, len(string) - 1, 1):
-    for j in range(i + 1, len(string), 1):
-      if string[i] == string[j]:
-        return False
+  """
+  Determine if string has all unique characters.
+  @param string: Input string
+  @return: True if string has all unique characters, else False
+  """
+  dictionary = {}
+  for i in range(0, len(string), 1):
+    if string[i] in dictionary:
+      return False
+    else:
+      dictionary[string[i]] = 1
   return True
+
+# O(n)
+def is_substring(string1, string2):
+  """
+  Determine if string1 is a substring of string2.
+  @param string1: Input substring
+  @param string2: Input string
+  @return: True if string1 is a substring of string2, else False
+  """
+  difference = len(string2) - len(string1)
+  if difference < 0:
+    return False
+  for i in range(0, difference + 1, 1):
+    substring = string2[i:i+len(string1)]
+    #print 'comparing', string1, 'to', substring
+    if string1 == substring:
+      return True
+  return False
--- unit_tests/test_stringops.py
+++ unit_tests/test_stringops.py
@@ -6,10 +6,19 @@ class TestStringOps(unittest.TestCase):
   def test_reverse(self):
     self.assertEqual('olleh', reverse('hello'))
     self.assertEqual('12345', reverse('54321'))
+    self.assertEqual('lol', reverse('lol'))
     
   def test_has_unique_chars(self):
     self.assertTrue(has_unique_chars('world'))
     self.assertFalse(has_unique_chars('hello'))
+    self.assertFalse(has_unique_chars('lol'))
     
+  def test_is_substring(self):
+    self.assertTrue(is_substring('h', 'h'))
+    self.assertTrue(is_substring('he', 'hello'))
+    self.assertFalse(is_substring('hello!', 'hello'))
+    self.assertFalse(is_substring('helo', 'hello'))
+    self.assertFalse(is_substring('hello', 'world'))
+  
 if __name__ == '__main__':
     unittest.main()
